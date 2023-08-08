06c3047c5333024fa4293d4f7bce5337b60546d8
Gabriel Dobato <dobatog@gmail.com>
2023-08-08 23:20:22 +0200

    Add blinky

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+#![no_std]
+
+pub mod panic;
+use embassy_stm32::{
+    gpio::{Level, Output, Speed},
+    peripherals,
+};
+
+use core::sync::atomic::{AtomicBool, Ordering};
+
+pub struct Board<'a> {
+    pub led_red: Output<'a, peripherals::PK5>,
+    pub led_green: Output<'a, peripherals::PK6>,
+    pub led_blue: Output<'a, peripherals::PK7>,
+}
+
+impl<'a> Board<'a> {
+    pub fn take() -> Self {
+        static TAKEN: AtomicBool = AtomicBool::new(false);
+        debug_assert!(!TAKEN.swap(true, Ordering::SeqCst));
+        Self::setup()
+    }
+
+    pub fn setup() -> Self {
+        let p = embassy_stm32::init(Default::default());
+        let led_red = Output::new(p.PK5, Level::High, Speed::Low);
+        let led_green = Output::new(p.PK6, Level::High, Speed::Low);
+        let led_blue = Output::new(p.PK7, Level::High, Speed::Low);
+
+        Self {
+            led_red,
+            led_green,
+            led_blue,
+        }
+    }
+}
